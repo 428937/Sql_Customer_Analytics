9ea928517918e61549f33ee2aa2daec2959b4c27
428937 <alpkaanayas@gmail.com>
2026-05-27 13:31:47 +0300

    Update 00_schema.sql

--- sql/00_schema.sql
+++ sql/00_schema.sql
@@ -19,5 +19,5 @@ CREATE TABLE orders (
 );
 
 CREATE INDEX idx_orders_customer_date ON orders(customer_id, order_date);
-CREATE INDEX idx_orders_order_date ON orders(order_date);
-CREATE INDEX idx_customers_signup ON customers(signup_date);
+CREATE INDEX idx_orders_order_date    ON orders(order_date);
+CREATE INDEX idx_customers_signup     ON customers(signup_date);
